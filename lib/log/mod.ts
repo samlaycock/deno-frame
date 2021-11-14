244d639df67608496447ebb13f7234123abd9a25
Samuel Laycock <samuel.paul.laycock@gmail.com>
2021-11-14 13:02:21 +0000

    feat(log): add more comprehensive export values to log module

--- lib/log/mod.ts
+++ lib/log/mod.ts
@@ -1,9 +1,9 @@
-import * as log from "./logger.ts";
+import log, { debug, error, info, warn } from "./logger.ts";
 import { LOG_DRIVERS, LOG_LEVELS } from "./constants.ts";
 import type { LogDriver, LogDriverType, LoggerOptions } from "./types.d.ts";
 
 export default log;
 
-export { LOG_DRIVERS, LOG_LEVELS };
+export { debug, error, info, LOG_DRIVERS, LOG_LEVELS, warn };
 
 export type { LogDriver, LogDriverType, LoggerOptions };
