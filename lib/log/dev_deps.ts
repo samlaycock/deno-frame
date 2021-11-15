800bc671d61392165a0234f20f64204341e23910
Samuel Laycock <samuel.paul.laycock@gmail.com>
2021-11-15 20:25:20 +0000

    chore(log): update mock library version

--- lib/log/dev_deps.ts
+++ lib/log/dev_deps.ts
@@ -1,4 +1,4 @@
 import * as asserts from "https://deno.land/std@0.113.0/testing/asserts.ts";
-import * as mock from "https://deno.land/x/mock@0.10.1/spy.ts";
+import * as mock from "https://deno.land/x/mock@0.12.0/spy.ts";
 
 export { asserts, mock };
