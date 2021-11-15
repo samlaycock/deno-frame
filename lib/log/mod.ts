dfa2bae42bb1ad2bf8f129b59d87658545b1b18b
Samuel Laycock <samuel.paul.laycock@gmail.com>
2021-11-15 20:24:46 +0000

    chore(log): clean up log module exports

--- lib/log/mod.ts
+++ lib/log/mod.ts
@@ -1,9 +1,4 @@
-import log, { debug, error, info, warn } from "./logger.ts";
-import { LOG_DRIVERS, LOG_LEVELS } from "./constants.ts";
-import type { LogDriver, LogDriverType, LoggerOptions } from "./types.d.ts";
-
-export default log;
-
-export { debug, error, info, LOG_DRIVERS, LOG_LEVELS, warn };
-
-export type { LogDriver, LogDriverType, LoggerOptions };
+export { default as log } from "./logger.ts";
+export { debug, error, info, warn } from "./logger.ts";
+export * from "./constants.ts";
+export * from "./types.d.ts";
