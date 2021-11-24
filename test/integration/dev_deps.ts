3b3f6c426af1804df618bb0cd9ba521520ed0541
Sam Laycock <samuel.paul.laycock@gmail.com>
2021-11-24 15:50:39 +0000

    test: update integration test runtime and dependencies

--- test/integration/dev_deps.ts
+++ test/integration/dev_deps.ts
@@ -1,3 +1,4 @@
 import * as asserts from "https://deno.land/std@0.115.1/testing/asserts.ts";
+import * as streams from "https://deno.land/std@0.115.1/streams/conversion.ts";
 
-export { asserts };
+export { asserts, streams };
